801079fc77cfbf89994d58b7334c96d904e67704
nigaige <nicolas.gaige@free.fr>
2019-01-28 10:56:10 +0100

    remove unneeded file

--- Light.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nicol on 25/01/2019.
-//
-
-#include "header/Light.h"
--- Mesh.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by nicol on 25/01/2019.
-//
-
-#include "header/Mesh.h"
-
-CRTColor Mesh::get_color(){
-    return color;
-}
